c1aa57539a037ad59e34230a70ef56abc1850a0c
Ziadhisham288 <ziadabomadina288@gmail.com>
2024-10-15 22:23:09 +0300

    message

--- src/modules/order/order.controller.js
+++ src/modules/order/order.controller.js
@@ -3,6 +3,7 @@ import { AppError } from "../../utils/AppError.js";
 import cartModel from "../../../Database/models/cart.model.js";
 import productModel from "../../../Database/models/product.model.js";
 import orderModel from "../../../Database/models/order.model.js";
+import express from 'express'
 
 import Stripe from 'stripe';
 const stripe = new Stripe('sk_test_51Q9vGDGgm2zKD6DvJ6NYmCiJxGEwwl2pr0iUMAOWSWx7HmGkIq4ts4bxeo7zCT3SYPgZuh5UJPmrsia55Vt0DUxp00L8wVYMxJ');
@@ -83,3 +84,33 @@ export const onlinePayment = handleError(async (req, res, next) => {
 
   res.status(200).json({message : "Done", session})
 });
+
+const app = express()
+
+app.post('/webhook', express.raw({type: 'application/json'}), (req, res) => {
+  const sig = req.headers['stripe-signature'];
+
+  let event; 
+
+  try{
+    event = stripe.webhooks.constructEvent(req.body, sig, "whsec_UnHTarFpf96SW5KwHHxJXZ6wNdrTsM0o")
+  } catch (err){
+    return res.status(400).send(`webhook Error: ${err.message}`)
+  }
+
+
+  if(event.type == "checkout.session.completed"){
+    const checkoutSessionCompleted = event.data.object;
+    console.log("done")
+  } else {
+    console.log(`Unhandled event type ${event.type}`);
+  }
+
+
+  res.json({message : "done"})
+});
+
+app.listen(4242, () => console.log("Running on port 4242"))
+
+export const createOnlineOrder = handleError(async (req,res,next) => {
+})
