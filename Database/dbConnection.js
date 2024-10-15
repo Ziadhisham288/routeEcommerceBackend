6a8bb9e2d232be9c0bd333eefa4ab6b8a368a741
Ziadhisham288 <ziadabomadina288@gmail.com>
2024-10-16 00:00:13 +0300

    a

--- Database/dbConnection.js
+++ Database/dbConnection.js
@@ -2,6 +2,6 @@ import mongoose from 'mongoose'
 
 export const dbConnection = () => {
   mongoose.connect(process.env.DB_ONLINE_CONNECTION)
-  .then(() => console.log("Database connected"))
+  .then(() => console.log("Database connected", process.env.DB_ONLINE_CONNECTION))
   .catch((err) => console.log(err))
 }
